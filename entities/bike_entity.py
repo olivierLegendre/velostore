9a74fb37cefad75776cf1a3cf4212a79e2981a52
olivier <olivier.legendre.pro@gmail.com>
2025-05-20 14:29:24 +0200

    refactor(database): :art: move database.py to db/
    move database.py to db directory

--- entities/bike_entity.py
+++ entities/bike_entity.py
@@ -1,5 +1,5 @@
 import os, sys
-sys.path.insert(1, "/".join(os.path.realpath(__file__).split("/")[0:-2]))
+sys.path.insert(1, "/".join(os.path.realpath(__file__).split("/")[0:-2]) + "/db")
 import database as db
 
 class BikeEntity(db.VelostoreDatabase):
