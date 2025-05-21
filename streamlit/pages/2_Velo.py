f33a52d39be0e0af1f1af8085985f423cf3a9aa5
olivier <olivier.legendre.pro@gmail.com>
2025-05-21 15:18:34 +0200

    feat(streamlit): :art: base structure of the streamlit app
    create the velos, velo and panier pages for the streamlit MPA

---
+++ streamlit/pages/2_Velo.py
@@ -0,0 +1,59 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "Velostore: Votre velo",
+    page_icon="🚲"
+)
+
+st.write("# Choisissez votre Velo 🚲")
+
+img_path = "streamlit/static/"
+
+def get_one_bike():
+    bike = dict()
+    bike["brand"] = "rockrider"
+    bike["description"] = "Oh, le joli velo"
+    bike["price"] = 150
+    bike["image"] = "A30M.jpg"
+    bike["destination"] = "vtt"
+    return bike
+    
+
+bike = get_one_bike()
+
+def add_bike_item_to_cart():
+    # bike_brand = st.session_state.bike_brand
+    # color = st.session_state.bike_color
+    # size = st.session_state.bike_brand
+    # bike = Bike_item(bike_brand, color, size)
+    st.session_state.bike_item = bike
+    st.switch_page("pages/3_Panier.py")
+
+def bike_display(bike_brand):
+    bike = bike_brand
+    colors = list(["bleu", "rouge", "vert", "jaune", "orange"])
+    size = list(["S", "M", "L", "XL"])
+    st.title(bike["brand"])
+    st.subheader(bike["description"])
+    st.text(f"A partir de {bike["price"]}€")
+    st.image(f"{img_path}{bike["image"]}")
+    st.text(f"Categorie : {bike["destination"]}")
+    select_color = st.selectbox("Choisissez votre couleur", colors)
+    select_size = st.selectbox("Choisissez votre taille", size)
+    st.session_state.bike_brand = bike_brand
+    st.session_state.bike_color = select_color
+    st.session_state.bike_brand = select_size
+    # st.button("Acheter", on_click=add_bike_item_to_cart)
+    if st.button("Acheter"):
+        # bike = Bike_item(bike_brand, color, size)
+        st.session_state.bike_item = bike
+        st.switch_page("pages/3_Panier.py")
+    
+bike_display(bike)
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ streamlit/pages/3_Panier.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "Velostore: Votre panier",
+    page_icon="🚲"
+)
+
+st.write("# Votre Panier 🚲")
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
