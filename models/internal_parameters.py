eefea47dbf37876c1aeac3db016ba6eab960c28f
elias <jean-pierre.elias@hotmail.fr>
2025-05-22 13:31:44 +0200

    feat: :sparkles: Creation of Internal parameter list
    - creation parameter list entity, - creation parameter list model, parameter entity modification, add method in parameters  model

--- models/internal_parameters.py
+++ models/internal_parameters.py
@@ -9,9 +9,43 @@ class InternalParameters():
         # self.cursor = super().cursor
         return
 
+    def get_bike_status_id(self, bike_status_id):
+        bike_status = self.entity.get_bike_status_by_id(bike_status_id)
+        print(bike_status)
+
+    def get_bike_size_id(self, size_id):
+        size = self.entity.get_bike_size_by_id(size_id)
+        print(size)
+
+    def get_destination_id(self, destination_id):
+        destination = self.entity.get_destination_by_id(destination_id)
+        print(destination)
+
+    def get_bike_color_id(self, color_id):
+        color = self.entity.get_bike_color_by_id(color_id)
+        print(color)
+
+    def get_order_status_id(self, order_status_id):
+        order_status = self.entity.get_order_status_by_id(order_status_id)
+        print(order_status)
+
+    def get_user_type_id(self, user_type_id):
+        user_type = self.entity.get_user_type_by_id(user_type_id)
+        print(user_type)
+
+    def get_user_status_id(self, user_status_id):
+        user_status = self.entity.gget_user_status_by_id(user_status_id)
+        print(user_status)
+
+    def get_statistics_name_id(self, statistics_name_id):
+        statistics_name = self.entity.get_statistics_name_by_id(statistics_name_id)
+        print(statistics_name)
 
 def main():
-    pass
+    internal_parameters_entity = ipe.InternalParametersEntity()
+    parameters = InternalParameters(internal_parameters_entity)
+    parameters.get_bike_size_id(2)  
+
 
 
 
