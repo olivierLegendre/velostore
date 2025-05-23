558c2415eb05307e2c0f4abd3c516ee2043d03ed
elias <jean-pierre.elias@hotmail.fr>
2025-05-23 15:29:23 +0200

    feat: :sparkles: create method in order
    create method in order list entity and order entity

--- models/order.py
+++ models/order.py
@@ -8,15 +8,29 @@ class Order():
         self.entity = entity
 
     def get_id(self, order_id):
-        order_id = self.entity.get_order_by_id(order_id)
-        print(order_id)
-        return order_id
+        return self.entity.get_order_by_id(order_id)
+    
+    def get_item_list_by_id(self, item_list_id):
+        return self.entity.get_item_list_by_id(item_list_id)
+    
+    def get_order_item_by_id(self, order_item_id):
+        return self.entity.get_item_list_by_id(order_item_id)
+    
+    def add_order_item(self, id_bike, nb_unit, total_price):
+        return self.entity.add_order_item(id_bike, nb_unit, total_price)
+    
+    def add_order(self, id_user, date, total_price, status):
+        return self.entity.add_order(id_user, date, total_price, status)
 
 
 def main():
     order_entity= oe.OrderEntity()
     order = Order(order_entity)
-    order_id = order.get_id(2) 
+    order.get_id(2)
+    order.get_item_list_by_id(2)
+    order.get_order_item_by_id(2)
+
+
 
 if __name__ == '__main__':
     main()
