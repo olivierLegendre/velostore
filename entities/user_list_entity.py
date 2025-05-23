015053a5cff699733340bce0ea539bf0e344b0aa
CVincent27 <cardon.vincent@hotmail.fr>
2025-05-23 14:10:57 +0200

    feat: add function get_user_list

--- entities/user_list_entity.py
+++ entities/user_list_entity.py
@@ -2,6 +2,12 @@ import os, sys
 sys.path.insert(1, "/".join(os.path.realpath(__file__).split("/")[0:-2]) + "/db")
 import database as db
 
+class UserListEntity(db.VelostoreDatabase):
+    def get_all_user_list(self):
+        self.cursor.execute("""SELECT * FROM user""")
+        return super().list_change()
+
+
 def main():
     pass
 
