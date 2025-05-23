f17d006b5550e45822dcc44c83e389c88831d868
olivier <olivier.legendre.pro@gmail.com>
2025-05-23 09:58:56 +0200

    feat(streamlit): :construction: test authenticator
    test streamlit authenticator

--- streamlit/components.py
+++ streamlit/components.py
@@ -1,4 +1,37 @@
 import streamlit as st
+import yaml
+import streamlit_authenticator as stauth
+from yaml.loader import SafeLoader
+
+
+
+def auth_login():
+    with open('.streamlit/auth_config.yaml') as file:
+        config = yaml.load(file, Loader=SafeLoader)
+
+        # Pre-hashing all plain text passwords once
+        # stauth.Hasher.hash_passwords(config['credentials'])
+
+        authenticator = stauth.Authenticate(
+            config['credentials'],
+            config['cookie']['name'],
+            config['cookie']['key'],
+            config['cookie']['expiry_days']
+        )
+        
+        try:
+            authenticator.login()
+        except Exception as e:
+            st.error(e)
+            
+        if st.session_state.get('authentication_status'):
+            authenticator.logout()
+            st.write(f'Welcome *{st.session_state.get("name")}*')
+            st.title('Some content')
+        elif st.session_state.get('authentication_status') is False:
+            st.error('Username/password is incorrect')
+        elif st.session_state.get('authentication_status') is None:
+            st.warning('Please enter your username and password')
 
 def display_bike(bike_brand):
     bike = bike_brand
--- streamlit/pages/5_Login.py
+++ streamlit/pages/5_Login.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import components
+import streamlit_authenticator as stauth
 
 def main():
     components.display_sidebar()
-    components.display_login_block()
+    # components.display_login_block()
+    components.auth_login()
     pass
 
 if __name__ == '__main__':
