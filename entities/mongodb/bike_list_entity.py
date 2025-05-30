a50936e876b4c1cae99e70d35f13a72c56060c16
VincentC27 <18120867+CVincent27@users.noreply.github.com>
2025-05-30 14:39:50 +0200

    feat: add bike list function

--- entities/mongodb/bike_list_entity.py
+++ entities/mongodb/bike_list_entity.py
@@ -15,20 +15,25 @@ class BikeListEntity(db.VelostoreDatabase):
         super().__init__()
         self.bike_collection = self.mydb['Bike']
     
-    def get_all_bike_list(self, bike_id):
-        bike_by_id = self.bike_collection.find_one({"_id": ObjectId(bike_id)})
-        print(bike_by_id)
-        return bike_by_id
-    
+    def get_all_bike_list(self):
+        """Récupère et affiche tous les vélos."""
+        all_bikes = list(self.bike_collection.find({}))
+        for bike in all_bikes:
+            print(bike)
+        return all_bikes
+
     def get_all_brand_list(self, expand=True):
+        """Récupère et affiche toutes les marques."""
         if expand:
-            all_brand = self.bike_collection.find_one({"brand": "Trek"})
-            print(all_brand)
+            all_brands = self.bike_collection.distinct("brand")
+            for brand in all_brands:
+                print(brand)
+            return all_brands
 
 def main():
     """Fonction principale pour la classe BikeListEntity."""
     super_velo = BikeListEntity()
-    super_velo.get_all_brand_list()
+    super_velo.get_all_bike_list()
 
 if __name__ == '__main__':
     main()
